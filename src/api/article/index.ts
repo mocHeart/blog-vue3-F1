7f510592f5754902a653f23aa641aa823dc3b78a
mocHeart <nuc2015@163.com>
2024-08-05 16:53:00 +0800

    文章样式和点赞，赞赏

--- src/api/article/index.ts
+++ src/api/article/index.ts
@@ -5,9 +5,14 @@ import { ArticleResp } from "@/api/article/type";
 // 通过枚举管理文章模块的接口地址
 enum API {
   ARTICLE_DETAIL_URL = "/articles/",
+  ARTICLE_COMMENTS_URL = "/comments/",
 }
 
 // 获取文章数据
 export const reqArticleDetail = (article_id: number) =>
   request.get<any, ArticleResp>(API.ARTICLE_DETAIL_URL + `${article_id}`);
+
+// 获取文章评论数据
+export const reqArticleComments = (cur: number, type: number, topicId:number) =>
+  request.get<any, ArticleResp>(API.ARTICLE_COMMENTS_URL + `?current=${cur}&type=${type}&topicId=${topicId}`);
 
--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -62,19 +62,115 @@
         </div>
       </div>
     </div>
-    <div style="height: 1000px; background-color: aquamarine">12222</div>
+    <!-- 内容 -->
+    <v-row class="article-container" style="border: 2px solid darkcyan">
+      <!-- 左边文章主体 -->
+      <v-col md="9" cols="12">
+        <v-card class="article-wrapper" elevation="10" rounded-lg>
+          <!--  Markdown文章  -->
+          <article
+            class="article-content markdown-body"
+            v-html="articleContent"
+            ref="article"
+          />
+          <!-- 版权声明 -->
+          <div class="aritcle-copyright">
+            <div>
+              <span>文章作者：</span>
+              <router-link to="/">
+                {{ homeStore.indexInfo.websiteConfig.websiteAuthor }}
+              </router-link>
+            </div>
+            <div>
+              <span>文章链接：</span>
+              <a :href="articleHref" target="_blank">{{ articleHref }} </a>
+            </div>
+            <div>
+              <span>版权声明：</span>本博客所有文章除特别声明外，均采用
+              <a
+                href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+                target="_blank"
+              >
+                CC BY-NC-SA 4.0
+              </a>
+              许可协议。转载请注明文章出处。
+            </div>
+          </div>
+          <!-- 标签和分享(未实现) -->
+          <div class="article-operation">
+            <div class="tag-container">
+              <router-link
+                v-for="item of tagDTOList"
+                :key="item.id"
+                :to="'/tags/' + item.id"
+              >
+                {{ item.tagName }}
+              </router-link>
+            </div>
+          </div>
+          <!-- 点赞打赏等 -->
+          <div class="article-reward">
+            <!-- 点赞按钮 -->
+            <a :class="isLike" @click="cLick">
+              <v-icon icon="mdi-thumb-up" size="small" color="#fff" /> 点赞
+              <span v-show="1 > 0">{{ 1 + 1 }}</span>
+            </a>
+            <a
+              class="reward-btn"
+              v-if="homeStore.indexInfo.websiteConfig.isReward == 0"
+            >
+              <!-- 打赏按钮 -->
+              <v-icon icon="mdi-qrcode" size="small" color="#fff" /> 打赏
+              <!-- 二维码 -->
+              <div class="animate__animated animate__fadeInDown reward-main">
+                <ul class="reward-all">
+                  <li class="reward-item">
+                    <img
+                      class="reward-img"
+                      src="https://macwoss.oss-cn-beijing.aliyuncs.com/config/16449fed803e665ebecb3055a5c992e2.jpg"
+                    />
+                    <div class="reward-desc">微信</div>
+                  </li>
+                  <li class="reward-item">
+                    <img
+                      class="reward-img"
+                      src="https://macwoss.oss-cn-beijing.aliyuncs.com/config/16449fed803e665ebecb3055a5c992e2.jpg"
+                    />
+                    <div class="reward-desc">支付宝</div>
+                  </li>
+                </ul>
+              </div>
+            </a>
+          </div>
+          <!-- 分割线 -->
+          <hr />
+        </v-card>
+      </v-col>
+      <!-- 侧边功能 -->
+      <v-col
+        md="3"
+        cols="12"
+        class="d-md-block d-none"
+        style="border: 2px solid blue"
+      >
+        2222
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script setup lang="ts" name="Article">
+import useHomeStore from "@/store/modules/home";
 import { reqArticleDetail } from "@/api/article";
-import { ArticleResp } from "@/api/article/type";
+import { ArticleResp, TagDTO } from "@/api/article/type";
 import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { getConvertDate } from "@/utils/timeUtil";
 import { md } from "@/plugins/markdown-it";
+import "@/style/markdown.scss";
 
 const route = useRoute();
+let homeStore = useHomeStore();
 
 let articleId = 0;
 let articleCover = ref("");
@@ -88,6 +184,8 @@ let wordNum = ref(0);
 let readTime = ref("");
 let viewsCount = ref(0);
 let commentCount = ref(0);
+let articleHref = ref(window.location.href);
+let tagDTOList = ref([] as TagDTO[]);
 onMounted(() => {
   articleId = parseInt(route.params?.articleId as string);
   getArticle();
@@ -106,6 +204,9 @@ const getArticle = async () => {
   wordNum.value = deleteHTMLTag(articleContent.value).length;
   readTime.value = Math.round(wordNum.value / 300) + "分钟";
   viewsCount.value = result.data.viewsCount;
+  result.data.tagDTOList.forEach((it: TagDTO) => {
+    tagDTOList.value.push(it);
+  });
 };
 
 let cover = computed(() => {
@@ -122,9 +223,30 @@ const deleteHTMLTag = (content: string) => {
     .replace(/[|]*\n/, "") // 去除行尾空格
     .replace(/&npsp;/gi, ""); // 去除空格
 };
+
+let like = ref(false);
+let isLike = computed(() => {
+  return like.value ? "like-btn-active" : "like-btn";
+});
+let cLick = () => {
+  like.value = !like.value;
+};
 </script>
 
 <style scoped lang="scss">
+@keyframes main {
+  0% {
+    opacity: 0;
+    filter: alpha(opacity=0);
+    transform: translateY(50px);
+  }
+  100% {
+    opacity: 1;
+    filter: none;
+    transform: translateY(0);
+  }
+}
+
 .article {
   position: relative;
   flex: 1 1 auto;
@@ -165,8 +287,167 @@ const deleteHTMLTag = (content: string) => {
           color: #fff !important;
           margin-left: 4px;
         }
-        .first-line, .first-line {
+        .first-line,
+        .first-line {
           margin: 5px auto;
+        }
+      }
+    }
+  }
+
+  .article-container {
+    animation: main 1s;
+    max-width: 1300px;
+    margin: 25px auto 0;
+    padding: 0 10px;
+    .article-wrapper {
+      padding: 50px 40px;
+      border-radius: 10px;
+      .aritcle-copyright {
+        position: relative;
+        margin-top: 35px;
+        margin-bottom: 10px;
+        font-size: 0.875rem;
+        line-height: 2;
+        padding: 0.625rem 1rem;
+        border: 1px solid #eee;
+        span {
+          color: #49b1f5;
+          font-weight: bold;
+        }
+        a {
+          text-decoration: underline !important;
+          color: #99a9bf !important;
+        }
+        &::before {
+          position: absolute;
+          top: 0.7rem;
+          right: 0.7rem;
+          width: 1rem;
+          height: 1rem;
+          border-radius: 1rem;
+          background: #49b1f5;
+          content: "";
+        }
+        &::after {
+          position: absolute;
+          top: 0.95rem;
+          right: 0.95rem;
+          width: 0.5rem;
+          height: 0.5rem;
+          border-radius: 0.5em;
+          background: #fff;
+          content: "";
+        }
+      }
+      .article-operation {
+        display: flex;
+        align-items: center;
+        .tag-container {
+          a {
+            display: inline-block;
+            margin: 0.5rem 0.5rem 0.5rem 0;
+            padding: 0 0.75rem;
+            width: fit-content;
+            border: 1px solid #49b1f5;
+            border-radius: 1rem;
+            color: #49b1f5 !important;
+            font-size: 12px;
+            line-height: 2;
+            &:hover {
+              color: #fff !important;
+              background: #49b1f5;
+              transition: all 0.5s;
+            }
+          }
+        }
+      }
+      .article-reward {
+        margin-top: 5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .like-btn {
+          display: inline-block;
+          width: 100px;
+          background: #969696;
+          color: #fff !important;
+          text-align: center;
+          line-height: 36px;
+          font-size: 0.875rem;
+        }
+        .like-btn-active {
+          display: inline-block;
+          width: 100px;
+          background: #ec7259;
+          color: #fff !important;
+          text-align: center;
+          line-height: 36px;
+          font-size: 0.875rem;
+        }
+        .reward-btn {
+          position: relative;
+          display: inline-block;
+          width: 100px;
+          background: #49b1f5;
+          margin: 0 1rem;
+          color: #fff !important;
+          text-align: center;
+          line-height: 36px;
+          font-size: 0.875rem;
+          .reward-main {
+            display: none;
+            position: absolute;
+            bottom: 40px;
+            left: 0;
+            margin: 0;
+            padding: 0 0 15px;
+            width: 100%;
+            .reward-all {
+              display: inline-block;
+              margin: 0 0 0 -110px;
+              padding: 20px 10px 8px !important;
+              width: 320px;
+              border-radius: 4px;
+              background: #f5f5f5;
+              &::before {
+                position: absolute;
+                bottom: -10px;
+                left: 0;
+                width: 100%;
+                height: 20px;
+                content: "";
+              }
+              &::after {
+                content: "";
+                position: absolute;
+                right: 0;
+                bottom: 2px;
+                left: 0;
+                margin: 0 auto;
+                width: 0;
+                height: 0;
+                border-top: 13px solid #f5f5f5;
+                border-right: 13px solid transparent;
+                border-left: 13px solid transparent;
+              }
+              .reward-item {
+                display: inline-block;
+                padding: 0 8px;
+                list-style-type: none;
+                .reward-img {
+                  width: 130px;
+                  height: 130px;
+                  display: block;
+                }
+              }
+            }
+          }
+          &:hover {
+            .reward-main {
+              display: block;
+            }
+          }
         }
       }
     }
